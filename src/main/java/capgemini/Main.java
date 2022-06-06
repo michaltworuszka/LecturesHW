cad088e765b454cc899555599bf93d7942c93acb
michaltworuszka <michal.tworuszka@gmail.com>
2022-06-06 12:16:58 +0200

    Added RandomGeneratorTest - test class which test if numbers are randomly generated

--- src/main/java/capgemini/Main.java
+++ src/main/java/capgemini/Main.java
@@ -3,6 +3,10 @@ package capgemini;
 public class Main {
     public static void main(String[] args) {
 
+        RandomGenerator randomGenerator = new RandomGenerator();
+
+      int number = randomGenerator.generate(10);
+        System.out.println(number);
 
     }
 }
